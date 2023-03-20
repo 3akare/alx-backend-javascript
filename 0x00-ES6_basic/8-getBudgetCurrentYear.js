e87684dee21975c33917fd6b5ef6a6a12ca534f0
David Bakare <bakaredavid007@gmail.com>
2023-03-20 19:23:32 +0100

    8-getBudgetCurrentYear.js && test

--- 0x00-ES6_basic/8-getBudgetCurrentYear.js
+++ 0x00-ES6_basic/8-getBudgetCurrentYear.js
@@ -3,14 +3,10 @@ function getCurrentYear() {
   return date.getFullYear();
 }
 
-function getBudgetForCurrentYear(income, gdp, capita) {
-  const budget = {};
-
-  budget[`income-${getCurrentYear()}`] = income;
-  budget[`gdp-${getCurrentYear()}`] = gdp;
-  budget[`capita-${getCurrentYear()}`] = capita;
-
-  return budget;
+export default function getBudgetForCurrentYear(income, gdp, capita) {
+  return {
+    [`income-${getCurrentYear()}`]: income,
+    [`gdp-${getCurrentYear()}`]: gdp,
+    [`capita-${getCurrentYear()}`]: capita,
+  };
 }
-
-console.log(getBudgetForCurrentYear(2100, 5200, 1090));
