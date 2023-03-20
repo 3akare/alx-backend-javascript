c7f55024050568f36140ad22b747920bccfe8f19
David Bakare <bakaredavid007@gmail.com>
2023-03-20 21:35:11 +0100

    semistandard check

--- 0x00-ES6_basic/test/8-getBudgetCurrentYear.test.js
+++ 0x00-ES6_basic/test/8-getBudgetCurrentYear.test.js
@@ -1,12 +1,12 @@
 import getBudgetForCurrentYear from '../8-getBudgetCurrentYear';
 
-function getCurrentYear() {
-    const date = new Date();
-    return date.getFullYear();
-  }
-  
+function getCurrentYear () {
+  const date = new Date();
+  return date.getFullYear();
+}
+
 test('getBudgetForCurrentYear', () => {
-    const year = getCurrentYear();
-    const mock = { [`income-${year}`]: 200, [`gdp-${year}`] : 300, [`capita-${year}`] : 400 };
+  const year = getCurrentYear();
+  const mock = { [`income-${year}`]: 200, [`gdp-${year}`]: 300, [`capita-${year}`]: 400 };
   expect(getBudgetForCurrentYear(200, 300, 400)).toEqual(mock);
 });
