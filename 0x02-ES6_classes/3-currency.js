0fefa33d29b3e96fdf60c8f6ee84a0f6e7c09e4a
David Bakare <bakaredavid007@gmail.com>
2023-03-23 17:22:23 +0100

    Fixed ESlint Issue

--- 0x02-ES6_classes/3-currency.js
+++ 0x02-ES6_classes/3-currency.js
@@ -1,30 +1,32 @@
-export default class Currency{
-    constructor(code, name){
-        if (typeof code !== 'string')
-        {
-            throw TypeError('Code must be a string');
-        }
-        if (typeof name != 'string')
-        {
-            throw TypeError('Name must be a string');
-        }
-        this._code = code;
-        this._name = name;
+export default class Currency {
+  constructor(code, name) {
+    if (typeof code !== 'string') {
+      throw TypeError('Code must be a string');
     }
-    get name(){
-        return this._name;
+    if (typeof name !== 'string') {
+      throw TypeError('Name must be a string');
     }
-    get code(){
-        return this._code;
-    }
-    set name(name){
-        this._name = name;
-    }
-    set code(code){
-        this._code = code;
-    }
+    this._code = code;
+    this._name = name;
+  }
 
-    displayFullCurrency(){
-        return `${this._name} (${this._code})`
-    }
+  get name() {
+    return this._name;
+  }
+
+  get code() {
+    return this._code;
+  }
+
+  set name(name) {
+    this._name = name;
+  }
+
+  set code(code) {
+    this._code = code;
+  }
+
+  displayFullCurrency() {
+    return `${this._name} (${this._code})`;
+  }
 }
--- 0x02-ES6_classes/tests/3-currency.test.js
+++ 0x02-ES6_classes/tests/3-currency.test.js
@@ -1,29 +1,29 @@
-import Currency from "../3-currency";
+import Currency from '../3-currency';
 
-test('Currency init', ()=>{
-    const mock = new Currency('$', 'Dollars');
-    expect(mock.name).toBe('Dollars');
-    expect(mock.code).toBe('$');
+test('Currency init', () => {
+  const mock = new Currency('$', 'Dollars');
+  expect(mock.name).toBe('Dollars');
+  expect(mock.code).toBe('$');
 
-    const mock2 = new Currency('N', 'Naira');
-    expect(mock2.name).toBe('Naira');
-    expect(mock2.code).toBe('N');
-})
+  const mock2 = new Currency('N', 'Naira');
+  expect(mock2.name).toBe('Naira');
+  expect(mock2.code).toBe('N');
+});
 
-test('Currency throw error test',()=>{
-    expect(()=>new Currency(1,'Pounds')).toThrow(TypeError('Code must be a string'));
-    expect(()=>new Currency('£', 1)).toThrow(TypeError('Name must be a string'));
-})
+test('Currency throw error test', () => {
+  expect(() => new Currency(1, 'Pounds')).toThrow(TypeError('Code must be a string'));
+  expect(() => new Currency('£', 1)).toThrow(TypeError('Name must be a string'));
+});
 
-test('Currency set and get test', ()=>{
-    let mock = new Currency('$', 'Dollars');
-    mock.code = '£';
-    mock.name = 'Pounds'
-    expect(mock['_code']).toBe('£');
-    expect(mock['_name']).toBe('Pounds');
-})
+test('Currency set and get test', () => {
+  const mock = new Currency('$', 'Dollars');
+  mock.code = '£';
+  mock.name = 'Pounds';
+  expect(mock._code).toBe('£');
+  expect(mock._name).toBe('Pounds');
+});
 
-test('Currency displayFullCurrency test', ()=>{
-    let mock = new Currency('¢', 'Cents');
-    expect(mock.displayFullCurrency()).toBe('Cents (¢)');
-})
+test('Currency displayFullCurrency test', () => {
+  const mock = new Currency('¢', 'Cents');
+  expect(mock.displayFullCurrency()).toBe('Cents (¢)');
+});
