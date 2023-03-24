11c452c4c00a36c2dab25f239b6728defa70bc3d
David Bakare <bakaredavid007@gmail.com>
2023-03-24 02:23:06 +0100

    9-hoisting.js && holbertonClass && test

--- 0x02-ES6_classes/9-hoisting.js
+++ 0x02-ES6_classes/9-hoisting.js
@@ -1,6 +1,19 @@
-import HolbertonClass from './holbertonClass';
+export class HolbertonClass {
+  constructor(year, location) {
+    this._year = year;
+    this._location = location;
+  }
 
-export default class StudentHolberton {
+  get year() {
+    return this._year;
+  }
+
+  get location() {
+    return this._location;
+  }
+}
+
+export class StudentHolberton {
   constructor(firstName, lastName, holbertonClass) {
     this._firstName = firstName;
     this._lastName = lastName;
