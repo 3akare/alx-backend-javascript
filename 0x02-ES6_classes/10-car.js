73b409194f0227c483c662b33ff03aa57987dab0
David Bakare <bakaredavid007@gmail.com>
2023-03-24 02:54:05 +0100

    car && evcar

---
+++ 0x02-ES6_classes/10-car.js
@@ -0,0 +1,29 @@
+export default class Car {
+  constructor(brand, motor, color) {
+    this._brand = brand;
+    this._motor = motor;
+    this._color = color;
+  }
+
+  static get [Symbol.species]() {
+    return this;
+  }
+
+  cloneCar() {
+    const Species = this.constructor[Symbol.species];
+
+    return new Species();
+  }
+}
+class TestCar extends Car {}
+
+const tc1 = new TestCar('Nissan', 'Turbo', 'Pink');
+const tc2 = tc1.cloneCar();
+
+console.log(tc1);
+console.log(tc1 instanceof TestCar);
+
+console.log(tc2);
+console.log(tc2 instanceof TestCar);
+
+console.log(tc1 == tc2);
