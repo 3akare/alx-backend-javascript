41fe113d480e3be64ec91aa212e8df36fc1465b6
David Bakare <bakaredavid007@gmail.com>
2023-03-28 15:37:26 +0100

    5-typed_arrays.js && test

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -1,6 +1,10 @@
 export default function createInt8TypedArray(length, position, value) {
-  const buffer = new ArrayBuffer(length);
-  const view = new DataView(buffer, 0, 10);
-  view.setUint8(position, value);
-  return view;
+  if (position > length) {
+    throw Error('Position outside range');
+  } else {
+    const buffer = new ArrayBuffer(length);
+    const view = new DataView(buffer, 0, 10);
+    view.setUint8(position, value);
+    return view;
+  }
 }
--- 0x03-ES6_data_manipulation/tests/5-typed_arrays.test.js
+++ 0x03-ES6_data_manipulation/tests/5-typed_arrays.test.js
@@ -1,7 +1,8 @@
 import createInt8TypedArray from '../5-typed_arrays';
 
-test('createInt*TypedArray', () => {
+test('createIntTypedArray', () => {
   const mock = createInt8TypedArray(10, 2, 89);
   expect(typeof mock === 'object').toBe(true);
   expect(mock.buffer.byteLength.valueOf()).toEqual(10);
+  expect(() => createInt8TypedArray(12, 13, 90)).toThrow(new Error('Position outside range'));
 });
