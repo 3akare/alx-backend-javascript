59ae94d062d2201a83ced6f844ee12b3df7ba21d
David Bakare <bakaredavid007@gmail.com>
2023-03-28 17:14:40 +0100

    8-clean_set.js && test

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,15 +1,17 @@
 export default function cleanSet(set, startString) {
   let newString = '';
   const returnArray = [];
-  if (newString === startString) {
-    return newString;
+  if (!set || !startString || !(set instanceof Set) || typeof startString !== 'string') {
+    return '';
   }
 
-  for (const x of set.keys()) {
-    if (x.includes(startString)) {
+  for (const x of set.values()) {
+    if (x.startsWith(startString) && typeof x === 'string') {
       newString = x.substring(startString.length);
       returnArray.push(newString);
     }
   }
   return returnArray.join('-');
 }
+console.log(cleanSet(new Set(['bonjovi', 'bonaparte', 'bonappetit', 'banana']), 'bon'));
+console.log(cleanSet(new Set(['bonjovi', 'bonaparte', 'bonappetit', 'banana']), ''));
