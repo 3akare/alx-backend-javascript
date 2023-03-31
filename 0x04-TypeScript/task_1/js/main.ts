efccecdae76bab1c917db638e2fe14c63dfb084b
David Bakare <bakaredavid007@gmail.com>
2023-03-31 09:47:46 +0100

    task 1

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -6,7 +6,35 @@ export interface Teacher {
     [index:string] : string | number | boolean;
 }
 
-
 export interface Directors extends Teacher {
     numberOfReports : 17;
 }
+
+export function printTeacher(firstname : string, lastname : string) : string
+{
+    return `${firstname} ${lastname}`;
+}
+
+export interface StudentClassI {
+    _firstName : string;
+    _lastName : string;
+    workOnHomeWork() : string;
+    displayName() : string;
+}
+
+export class StudentClass implements StudentClassI{
+    _firstName : string;
+    _lastName : string;
+    constructor(firstName : string, lastName : string){
+        this._firstName = firstName;
+        this._lastName = lastName;
+    }
+
+    workOnHomeWork(): string {
+        return 'Currently working';
+    }
+
+    displayName(): string {
+        return `${this._firstName}`
+    }
+} 
