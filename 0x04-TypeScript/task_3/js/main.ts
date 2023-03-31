78cdfddc2b8d2f4af31abba192874f4b066f8580
David Bakare <bakaredavid007@gmail.com>
2023-03-31 14:43:50 +0100

    task 3

--- 0x04-TypeScript/task_3/js/main.ts
+++ 0x04-TypeScript/task_3/js/main.ts
@@ -1,63 +1,14 @@
-export interface DirectorInterface {
-    workFromHome() : string;
-    getToWork() : string;
-    workDirectorTasks() : string;
+/// <reference path="./crud.d.ts" />
+import { RowID, RowElement } from "./interface";
+import * as CRUD from "./crud";
+
+let row : RowElement = {
+    firstName: "Guillaume",
+    lastName: "Salva"
 }
 
-export interface TeacherInterface {
-    workFromHome() : string;
-    getCoffeeBreak() : string;
-    workTeacherTasks() : string;
-}
+const newRowID : RowID = CRUD.insertRow(row);
+const updatedRow = Object.assign(row, {age:23});
 
-export class Director implements DirectorInterface {
-    workFromHome(): string {
-        return 'Working from home';
-    }
-    getToWork() : string{
-        return 'coffee break';
-    }
-    workDirectorTasks(): string {
-        return 'getting to director tasks';
-    }
-}
-
-export class Teacher implements TeacherInterface{
-    workFromHome(): string {
-        return 'Cannot work from home';
-    }
-    getCoffeeBreak(): string {
-        return 'Cannot have a break';
-    }
-    workTeacherTasks(): string {
-        return 'Getting to work';
-    }
-}
-
-export function createEmployee(salary: number | string) : Director | Teacher{
-    if(typeof salary === 'number' && salary < 500){
-        return new Teacher;
-    }
-    return new Director;
-}
-
-
-export function isDirector(employee : Director | Teacher) : boolean{
-    const status = employee instanceof Director? true : false
-    return status;
-}
-
-export function executeWork(employee : any ) : void{
-    if(isDirector(employee)){
-        console.log(employee.workDirectorTasks());
-    }
-    else{
-        console.log(employee.workTeacherTasks());
-    }
-}
-
-type Subjects = "Math" | "History"
-
-export function teachClass(todayClass : Subjects) : string{
-    return `Teaching ${todayClass}`;
-}
+CRUD.updateRow(newRowID, updatedRow);
+CRUD.deleteRow(newRowID);
