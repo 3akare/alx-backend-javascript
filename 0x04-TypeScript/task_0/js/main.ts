4bf8ad9f040390006ab3fbb747f7bb5bddae7e08
David Bakare <bakaredavid007@gmail.com>
2023-03-31 08:47:52 +0100

    corrected non-inferrable-types error

--- 0x04-TypeScript/task_0/js/main.ts
+++ 0x04-TypeScript/task_0/js/main.ts
@@ -20,14 +20,14 @@ function createStudents(firstname : string, lastname : string, age : number, loc
 studentsList.push(createStudents("David", "Bakare", 9, "Abuja"));
 studentsList.push(createStudents("Samuel", "Ajibola", 10, "Ekiti"));
 
-function tableCreate() {
-    let html : string  = `<table><tr><th>First Name</th><th>Location</th></tr>`;
-    for (let x of studentsList){
+function tableCreate() : void{
+    let html = '<table><tr><th>First Name</th><th>Location</th></tr>';
+    for (const x of studentsList){
         html += `<tr><td>${x.firstname}</td><td>${x.location}</td></tr>`
     }
     html += '</table>';
 
-    const tbl = document.getElementsByTagName("BODY")[0].innerHTML = html
+    document.getElementsByTagName("BODY")[0].innerHTML = html
   }
   
   tableCreate();
