fcc8c248d79ca9add9b58e8d8c1068ecbea4cf4e
David Bakare <bakaredavid007@gmail.com>
2023-04-02 22:19:03 +0100

    all

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,13 +1,12 @@
 import { uploadphoto, createUser } from './utils';
 
 export default function handelProfileSignup() {
-  const photo = uploadphoto();
-  const user = createUser();
-  const all = Promise.all([photo, user]);
-  all.then((message) => {
-    console.log(`${message[0].body} ${message[1].firstName} ${message[1].lastName}`);
-  });
-  all.catch(() => {
-    throw Error('Signup system offline');
-  });
+  return Promise
+    .all([uploadphoto(), createUser()])
+    .then((message) => {
+      console.log(`${message[0].body} ${message[1].firstName} ${message[1].lastName}`);
+    })
+    .catch(() => {
+      throw Error('Signup system offline');
+    });
 }
