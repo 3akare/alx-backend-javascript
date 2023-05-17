df6cc9679e993696dd5ff7bf3925ad345f78d45d
David Bakare <bakaredavid007@gmail.com>
2023-05-17 23:25:32 +0100

    1-stdin.js

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,13 +1,15 @@
-/**
- * STDIN
- */
+const readline = require('readline');
 
-const readline = require('readline').createInterface({
+// Create the readline interface
+const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout
 });
 
-readline.question('Welcome to Holberton School, what is your name?\n', input => {
-  console.log(`Your name is: ${input}\nThis important software is now closing`);
-  readline.close();
+rl.question('Welcome to Holberton School, what is your name?\n', (name) => {
+  process.stdout.write(`Your name is: ${name}`);
+  if (!process.stdin.isTTY) {
+    process.stdout.write('\nThis important software is now closing');
+  }
+  rl.close();
 });
