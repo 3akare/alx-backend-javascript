c53cf59c2f6a7ff701267fb86cb5e10a81a2d815
David Bakare <bakaredavid007@gmail.com>
2023-05-29 12:52:00 +0100

    unitesting with mocha IX

--- 0x06-unittests_in_js/9-api/api.js
+++ 0x06-unittests_in_js/9-api/api.js
@@ -1,21 +1,12 @@
 const express = require('express');
 const app = express();
 
-// Middleware to validate :id parameter as a number
-const validateIdParam = (req, res, next) => {
-  const id = req.params.id;
-  if (!/^\d+$/.test(id)) {
-    return res.status(400).send('Invalid ID');
-  }
-  next();
-};
-
 // Routes
 app.get('/', (req, res) => {
   res.send('Welcome to the payment system');
 });
 
-app.get('/cart/:id', validateIdParam, (req, res) => {
+app.get('/cart/:id(\\d+)', (req, res) => {
   const cartId = req.params.id;
   res.send(`Payment methods for cart ${cartId}`);
 });
