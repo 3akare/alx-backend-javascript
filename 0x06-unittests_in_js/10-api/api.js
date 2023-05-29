e88c17a508fc46f05ca383c850b9e591e7dc7598
David Bakare <bakaredavid007@gmail.com>
2023-05-29 13:06:13 +0100

    unitesting with mocha XII

--- 0x06-unittests_in_js/10-api/api.js
+++ 0x06-unittests_in_js/10-api/api.js
@@ -1,30 +1,36 @@
 const express = require('express');
-const bodyParser = require('body-parser');
 
 const app = express();
-app.use(bodyParser.json());
+const PORT = 7865;
 
-// GET /available_payments endpoint
+app.use(express.json());
+
+app.get('/', (_req, res) => {
+  res.send('Welcome to the payment system');
+});
+
+app.get('/cart/:id(\\d+)', (req, res) => {
+  const id = req.params.id;
+
+  res.send(`Payment methods for cart ${id}`);
+});
+
 app.get('/available_payments', (_req, res) => {
-  const paymentMethods = {
-    payment_methods: {
-      credit_cards: true,
-      paypal: false,
-    },
-  };
-  res.json(paymentMethods);
+  res.json({ payment_methods: { credit_cards: true, paypal: false } });
 });
 
-// POST /login endpoint
 app.post('/login', (req, res) => {
-  const { userName } = req.body;
-  res.send(`Welcome ${userName}`);
+  let username = '';
+
+  if (req.body) {
+    username = req.body.userName;
+  }
+
+  res.send(`Welcome ${username}`);
 });
 
-// Start server
-const port = 7865;
-app.listen(port, () => {
-  console.log(`API available on localhost:${port}`);
+app.listen(PORT, () => {
+  console.log(`API available on localhost port ${PORT}`);
 });
 
 module.exports = app;
--- 0x06-unittests_in_js/10-api/api.test.js
+++ 0x06-unittests_in_js/10-api/api.test.js
@@ -4,6 +4,37 @@ const { expect } = require('chai');
 const serverUrl = 'http://localhost:7865'; // Update with the actual server URL
 
 describe('API integration tests', () => {
+  describe('GET /', () => {
+    it('returns welcome message', (done) => {
+      request.get(`${serverUrl}/`, (error, response, body) => {
+        expect(response.statusCode).to.equal(200);
+        expect(body).to.equal('Welcome to the payment system');
+        done();
+      });
+    });
+  });
+
+  describe('GET /cart/:id', () => {
+    it('returns payment methods for cart with valid ID', (done) => {
+      const cartId = 123;
+
+      request.get(`${serverUrl}/cart/${cartId}`, (error, response, body) => {
+        expect(response.statusCode).to.equal(200);
+        expect(body).to.equal(`Payment methods for cart ${cartId}`);
+        done();
+      });
+    });
+
+    it('returns 404 for cart with invalid ID', (done) => {
+      const invalidCartId = 'abc';
+
+      request.get(`${serverUrl}/cart/${invalidCartId}`, (error, response, body) => {
+        expect(response.statusCode).to.equal(404);
+        done();
+      });
+    });
+  });
+
   describe('GET /available_payments', () => {
     it('returns available payment methods', (done) => {
       request.get(`${serverUrl}/available_payments`, (error, response, body) => {
