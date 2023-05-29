d2d8d779efdb46008c1534bee3988b7e9ffeb78a
David Bakare <bakaredavid007@gmail.com>
2023-05-29 12:49:21 +0100

    unitesting with mocha IX

--- 0x06-unittests_in_js/9-api/api.js
+++ 0x06-unittests_in_js/9-api/api.js
@@ -1,23 +1,27 @@
 const express = require('express');
 const app = express();
-const port = 7865;
 
+// Middleware to validate :id parameter as a number
+const validateIdParam = (req, res, next) => {
+  const id = req.params.id;
+  if (!/^\d+$/.test(id)) {
+    return res.status(400).send('Invalid ID');
+  }
+  next();
+};
+
+// Routes
 app.get('/', (req, res) => {
   res.send('Welcome to the payment system');
 });
 
-app.get('/cart/:id', (req, res) => {
-  const id = req.params.id;
-
-  // Validate :id as a number
-  if (isNaN(id)) {
-    res.status(400).send('Invalid cart ID');
-  } else {
-    // Return payment methods for the cart
-    res.send(`Payment methods for cart ${id}`);
-  }
+app.get('/cart/:id', validateIdParam, (req, res) => {
+  const cartId = req.params.id;
+  res.send(`Payment methods for cart ${cartId}`);
 });
 
+// Start server
+const port = 7865;
 app.listen(port, () => {
   console.log(`API available on localhost:${port}`);
 });
