b05dc2fb715bcc7a59a84c2ee48bb3d44428f21b
David Bakare <bakaredavid007@gmail.com>
2023-05-29 12:45:11 +0100

    unitesting with mocha IX

--- 0x06-unittests_in_js/9-api/api.test.js
+++ 0x06-unittests_in_js/9-api/api.test.js
@@ -1,18 +1,8 @@
 const request = require('request');
 const { expect } = require('chai');
-const app = require('./api');
 
 describe('API integration test', () => {
   const API_URL = 'http://localhost:7865';
-
-  // Existing test for GET /
-  it('GET / returns correct response', (done) => {
-    request.get(API_URL, (_err, res, body) => {
-      expect(res.statusCode).to.equal(200);
-      expect(body).to.equal('Welcome to the payment system');
-      done();
-    });
-  });
 
   // New test suite for cart page
   describe('GET /cart/:id', () => {
