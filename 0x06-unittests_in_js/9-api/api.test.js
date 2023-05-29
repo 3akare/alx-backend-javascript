4c0652dd006083afab861fbb733a61f0f9e1c22a
David Bakare <bakaredavid007@gmail.com>
2023-05-29 13:19:34 +0100

    unitesting with mocha XI

--- 0x06-unittests_in_js/9-api/api.test.js
+++ 0x06-unittests_in_js/9-api/api.test.js
@@ -1,28 +1,22 @@
 const request = require('request');
 const { expect } = require('chai');
 
+const API_URL = 'http://localhost:7865';
+
 describe('API integration test', () => {
-  const API_URL = 'http://localhost:7865';
-
-  // New test suite for cart page
-  describe('GET /cart/:id', () => {
-    it('returns correct payment methods when :id is a number', (done) => {
-      const cartId = 123; // Example cart ID
-
-      request.get(`${API_URL}/cart/${cartId}`, (_err, res, body) => {
-        expect(res.statusCode).to.equal(200);
-        expect(body).to.equal(`Payment methods for cart ${cartId}`);
-        done();
-      });
+  it('GET / returns correct response', (done) => {
+    request.get(`${API_URL}/`, (_err, res, body) => {
+      expect(res.statusCode).to.be.equal(200);
+      expect(body).to.be.equal('Welcome to the payment system');
+      done();
     });
+  });
 
-    it('returns 400 status code when :id is not a number', (done) => {
-      const cartId = 'abc'; // Invalid cart ID
-
-      request.get(`${API_URL}/cart/${cartId}`, (_err, res, body) => {
-        expect(res.statusCode).to.equal(404);
-        done();
-      });
+  it('GET /cart/:id returns correct response for valid :id', (done) => {
+    request.get(`${API_URL}/cart/47`, (_err, res, body) => {
+      expect(res.statusCode).to.be.equal(200);
+      expect(body).to.be.equal('Payment methods for cart 47');
+      done();
     });
   });
 });
